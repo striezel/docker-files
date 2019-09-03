3584e1134ff79baee6961f96568740ecafc8597c
Dirk Stolle <striezel-dev@web.de>
2019-09-03 23:19:48 +0200

    add basic Dockerfile for .NET core SDK

---
+++ dotnet-core/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (C) 2019  Dirk Stolle
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+IMAGE="dotnet-sdk"
+
+cd "${BASH_SOURCE%/*}" || exit 1
+docker image inspect "$IMAGE" > /dev/null 2>&1
+if [[ $? -ne 0 ]]
+then
+  echo "Info: Image $IMAGE does not exist, building it now."
+  docker build -t "$IMAGE" .
+  if [[ $? -ne 0 ]]
+  then
+    echo "Docker image build process failed!"
+    exit 2
+  fi
+fi
+
+docker run --rm "$IMAGE"
