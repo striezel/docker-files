1b479774c3d6b25997fe05c6bd191de0028fb1e5
Dirk Stolle <striezel-dev@web.de>
2022-07-03 20:34:03 +0200

    adjustments for build scripts of older Node.js versions
    The Debian package repository for older Node.js versions only has
    packages for Debian 10 ("buster") but not for the current Debian
    11 ("bullseye"), so a separate Dockerfile handles those cases.

--- node/build_node_4.sh
+++ node/build_node_4.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build . -t node-04 --build-arg NODE_VERSION=4.x
+docker build . -t node-04 -f Dockerfile_buster --build-arg NODE_VERSION=4.x
--- node/build_node_8.sh
+++ node/build_node_8.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build . -t node-08 --build-arg NODE_VERSION=8.x
+docker build . -t node-08 -f Dockerfile_buster --build-arg NODE_VERSION=8.x
